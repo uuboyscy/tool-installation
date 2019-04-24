17fba225c232ffaf2056480b0bff388dff120ea1
uuboyscy <uuboyscy@localhost.localdomain>
2019-04-24 12:41:49 +0800

    docker

---
+++ docker-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+sleep 120
+
+sudo systemctl start docker.service
+
+sleep 20
+
+sudo systemctl enable docker.service
+
+sleep 20
+
+sudo usermod -aG docker `whoami`
+
+echo "docker installed."
+docker -v
