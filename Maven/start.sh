5999c62e33fb4fad23e76290c3ab48a5aac01712
uuboyscy <aegis12321@gmail.com>
2020-08-26 23:34:44 +0800

    maven starter and env path

--- Maven/start.sh
+++ Maven/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-IMG_NAME="$(docker images | grep uuboyscy/centos6.6 | head -n1 | awk -F' ' '{print $1}')"
+IMG_NAME="$(docker images | grep uuboyscy/maven-jdk1.8 | head -n1 | awk -F' ' '{print $1}')"
 
-if [ "X$IMG_NAME" != "Xuuboyscy/centos6.6" ]
+if [ "X$IMG_NAME" != "Xuuboyscy/maven-jdk1.8" ]
 then
     echo "Building image..."
-    docker build -t uuboyscy/centos6.6 .
+    docker build -t uuboyscy/maven-jdk1.8 .
 fi
 
-docker run -it --rm -v $(pwd)/git_dir:/work/git_dir uuboyscy/centos6.6 /bin/bash
+docker run -it --rm -v $(pwd)/git_dir:/work/git_dir uuboyscy/maven-jdk1.8 /bin/bash
