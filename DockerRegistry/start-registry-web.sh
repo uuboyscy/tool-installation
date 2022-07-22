3a1af57afbe3ed0dfe64c1b99da583b2c6bd7729
uuboyscy <aegis12321@gmail.com>
2022-07-22 16:49:41 +0800

    Update:

--- DockerRegistry/start-registry-web.sh
+++ DockerRegistry/start-registry-web.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -x
 
-docker run -d -p 8080:8080 --name registry-web --net dockerregistry_registry_network -e REGISTRY_URL=http://172.23.0.11:5000/v2 hyper/docker-registry-web
+docker run -it -d -p 8080:8080 --name registry-web \
+	--net dockerregistry_registry_network \
+	-v $(pwd)/conf/registry-web.yml:/conf/config.yml:ro \
+	-v $(pwd)/conf/auth.key:/conf/auth.key -v $(pwd)/db:/data \
+	-e REGISTRY_URL=http://172.23.0.11:5000/v2 \
+	-e REGISTRY_NAME=api.mydomain.com:5000 \
+	hyper/docker-registry-web
